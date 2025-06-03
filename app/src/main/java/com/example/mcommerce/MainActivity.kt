6430998efddc3ab6fac60e3b571eb6eb63086759
KhairySuleiman4 <khairy.hatem4@gmail.com>
2025-06-04 00:11:14 +0300

    initial commit

---
+++ app/src/main/java/com/example/mcommerce/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.mcommerce
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mcommerce.ui.theme.MCommerceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Test()
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun TestPreview() {
+    Test()
+}
+
+@Composable
+fun Test(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text("Hello M-Commerce")
+    }
+
+}
