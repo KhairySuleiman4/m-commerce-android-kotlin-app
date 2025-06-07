8279b3105e56126675cc67068b33ecfd126d9106
Hazem-web <hazem.mah2722@gmail.com>
2025-06-07 21:11:47 +0300

    add api dependency

--- app/src/main/java/com/example/mcommerce/data/remote/exchangerateapi/ExchangeService.kt
+++ app/src/main/java/com/example/mcommerce/data/remote/exchangerateapi/ExchangeService.kt
@@ -1,8 +1,10 @@
 package com.example.mcommerce.data.remote.exchangerateapi
 
+import com.example.mcommerce.data.models.ExchangeResponse
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface ExchangeService {
-    @GET("")
-    fun getEGPExchangeRate()
+    @GET("latest/EGP")
+    fun getEGPExchangeRate() : Response<ExchangeResponse>
 }
