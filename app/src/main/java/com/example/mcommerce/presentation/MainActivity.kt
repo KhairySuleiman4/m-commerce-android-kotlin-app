0ec1cbd41a96309ea818aace5c7209dd665b9995
Shereen Mohamed <shereenmohamed923@gmail.com>
2025-06-12 19:25:26 +0300

    control showing bottom navbar and navigation backstack handled

--- app/src/main/java/com/example/mcommerce/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mcommerce/presentation/MainActivity.kt
@@ -14,8 +14,6 @@ import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
