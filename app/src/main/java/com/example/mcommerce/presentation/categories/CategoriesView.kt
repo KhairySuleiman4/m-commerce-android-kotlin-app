9a3d80267250c69be03e9475eb8eb42287a42ee7
Shereen Mohamed <shereenmohamed923@gmail.com>
2025-06-15 08:48:06 +0300

    coupon and appbar design added

--- app/src/main/java/com/example/mcommerce/presentation/categories/CategoriesView.kt
+++ app/src/main/java/com/example/mcommerce/presentation/categories/CategoriesView.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -21,7 +20,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
